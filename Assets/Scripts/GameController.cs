174595a567cbc4b09dc9150136b1951adbed301f
mtri0001 <migueltomasri@gmail.com>
2015-05-05 01:02:30 +0200

    5-05-2015

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,16 +10,38 @@ public class GameController : MonoBehaviour {
 
 	public GameObject Player1Zone;
 	public GameObject Player2Zone;
+	public GameObject card;
 
 	public GameObject table;
+
+	private GameObject hand1;
 
 	private void Awake(){
 		initializeTable ();
 		StartGame ();
 	}
 
+	void Start(){
+
+
+	}
+
 	private void initializeTable(){
-		Instantiate(table);
+		//Instantiate(table);
+		/*for(int i = 0; i < elementsOfPlayer1.Length; ++i){
+			if(elementsOfPlayer1[i].name == "handPlayer1"){
+				Debug.Log("TOTUS");
+			}
+		}*/
+
+		GameObject[] elementsOfPlayer1 = GameObject.FindGameObjectsWithTag ("Player1");
+
+		foreach (GameObject go in elementsOfPlayer1)
+			if (go.name == "handPlayer1") {
+				GameObject c = (GameObject) Instantiate (card);
+				c.transform.SetParent(go.transform , false);
+			}
+
 	}
 
 	void StartGame(){
@@ -33,8 +55,6 @@ public class GameController : MonoBehaviour {
 			setPlayer2 = true;
 			//posicionar en setPlayer2 en PlayerZone2
 		}
-			
-
 	}
 
 
